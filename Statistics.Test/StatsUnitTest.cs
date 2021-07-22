97507a2025307e84b25cd9f5e5e8ba6f07e4fb95
Ajaydeep4 <46348665+Ajaydeep4@users.noreply.github.com>
2021-07-22 08:13:35 +0530

    Add files via upload
    Statictics.Tests files

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,7 +1,9 @@
 using System;
 using Xunit;
 using Statistics;
+using Alerts;
 
+using System.Collections.Generic;
 namespace Statistics.Test
 {
     public class StatsUnitTest
@@ -9,20 +11,20 @@ namespace Statistics.Test
         [Fact]
         public void ReportsAverageMinMax()
         {
-            var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+            //ICalculateStatistics statsComputer = new StatsComputer();
+            var computedStats = StatsComputer.CalculateStatistics(
+                new List<double> {1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+            var computedStats = StatsComputer.CalculateStatistics(
+                new List<double> { });
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
@@ -32,11 +34,11 @@ namespace Statistics.Test
         {
             var emailAlert = new EmailAlert();
             var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
-
-            const float maxThreshold = 10.2;
-            var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
+            IAlerter[] alerters = { emailAlert, ledAlert };
+            List<IAlerter> alertersList = new List<IAlerter>(alerters);
+            const double maxThreshold = 10.2;
+            var statsAlerter = new StatsAlerter(maxThreshold, alertersList);
+            statsAlerter.CheckAndAlert(new List<double> { 0.2, 11.9, 4.3, 8.5 });
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
