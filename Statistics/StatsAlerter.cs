9e7b41b9af1491f9b22cd62574e68b3aa4342f5b
Ajaydeep4 <46348665+Ajaydeep4@users.noreply.github.com>
2021-07-22 08:14:39 +0530

    Add files via upload
    Statistics files

---
+++ Statistics/StatsAlerter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alerts;
+
+
+namespace Statistics
+{
+    public class StatsAlerter
+    {
+        //public Action<double> alertHandeler = null;
+        double maxThreshold;
+        List<IAlerter> alertHandeler = new List<IAlerter>();
+        public StatsAlerter(double maxThreshold, List<IAlerter> alerters)
+        {
+            this.maxThreshold = maxThreshold;
+            for (int i = 0; i < alerters.Count; ++i)
+                AddAlerter(alerters[i]);
+        }
+        public void AddAlerter(IAlerter alerter)
+        {
+            alertHandeler.Add(alerter);
+        }
+        public void CheckAndAlert(List<double> numbers)
+        {
+            double max = StatsComputer.CalculateStatistics(numbers).max;
+            for (int i = 0; i < alertHandeler.Count; ++i)
+                alertHandeler[i].Alert(maxThreshold, max);
+        }
+    }
+}
---
+++ Statistics/StatsComputer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Alerts;
+
+namespace Statistics
+{
+    //Was thinking of using function pareadigm but decide to use OOP.
+
+    //public interface ICalculateStatistics
+    //{
+    //    Stats CalculateStatistics(List<double> numbers);
+    //}
+    
+    public class StatsComputer
+    {
+        static public StatsDataModel CalculateStatistics(List<double> numbers) {
+            //Implement statistics here
+            double average;
+            double max;
+            double min;
+            if (numbers.Count == 0)
+                return new StatsDataModel();
+            max = numbers[0];
+            min = numbers[0];
+            average = 0;
+            foreach(float number in numbers)
+            {
+                if (number > max)
+                    max = number;
+                if (number < min)
+                    min = number;
+                average += number;
+            }
+            average /= numbers.Count;
+            return ( new StatsDataModel(average, max, min) );
+        }
+    }
+    
+    //static Func<double, bool> CheckArgumentGreateThanDouble(double b)
+    //{
+    //    bool Check(double a)
+    //    {
+    //        return a>b;
+    //    }
+    //    return Check;
+    //}
+    
+}
+
