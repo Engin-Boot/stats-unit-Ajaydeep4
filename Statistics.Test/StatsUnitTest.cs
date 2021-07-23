1217f275170a02dd6409a26ab8c746ba64d07edb
Ajaydeep4 <ajaydeep44444@gmail.com>
2021-07-23 16:15:26 +0530

    Refractered code

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -39,19 +39,6 @@ namespace Statistics.Test
             Assert.True(Double.NaN.Equals(computedStats.max));
             Assert.True(Double.NaN.Equals(computedStats.min));
         }
-        [Fact]
-        public void RaisesAlertsIfMaxIsMoreThanThreshold()
-        {
-            var emailAlert = new EmailAlert();
-            var ledAlert = new LEDAlert();
-            IAlerter[] alerters = { emailAlert, ledAlert };
-            List<IAlerter> alertersList = new List<IAlerter>(alerters);
-            const double maxThreshold = 10.2;
-            var statsAlerter = new StatsAlerter(maxThreshold, alertersList);
-            statsAlerter.CheckAndAlert(new List<double> { 0.2, 11.9, 4.3, 8.5 });
-
-            Assert.True(emailAlert.emailSent);
-            Assert.True(ledAlert.ledGlows);
-        }
+        
     }
 }
